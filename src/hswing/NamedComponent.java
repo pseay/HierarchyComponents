23209f840099e8553d8589e9edf5febc595c9f5c
Preston Seay <preston.seay@outlook.com>
2021-02-09 21:04:10 -0800

    Added JavaDoc comments for NamedComponent.java

--- src/hswing/NamedComponent.java
+++ src/hswing/NamedComponent.java
@@ -2,22 +2,46 @@ package hswing;
 
 import javax.swing.*;
 
+/**
+ * Creates an association between a name and a {@link JComponent}.
+ * This name can be used later by a {@link Navigator} in order to retrieve the {@link JComponent}.
+ */
 public class NamedComponent {
-    private String name;
-    private JComponent component;
+    private final String name;
+    private final JComponent component;
+
+    /**
+     * Creates a new NamedComponent with the given name and {@link JComponent}
+     * @param name  the name of a component to be used by a {@link Navigator}
+     * @param component  the {@link JComponent} associated with the name
+     */
     private NamedComponent(String name, JComponent component) {
         this.name = name;
         this.component = component;
     }
 
+    /**
+     *
+     * @param name  the name for a new {@link NamedComponent}
+     * @param component  the component for a new {@link NamedComponent}
+     * @return  a new named component with the name and component
+     */
     public static NamedComponent named(String name, JComponent component) {
         return new NamedComponent(name, component);
     }
 
+    /**
+     * Gets the name
+     * @return  the name
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * Gets the component
+     * @return  the component
+     */
     public JComponent getComponent() {
         return component;
     }
