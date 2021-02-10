03f576fb2710cd32429a9deeee8f6aa4a9f30a8a
Preston Seay <preston.seay@outlook.com>
2021-02-09 22:39:41 -0800

    JavaDoc comments for HFrame.java

--- src/hswing/HFrame.java
+++ src/hswing/HFrame.java
@@ -2,20 +2,30 @@ package hswing;
 
 import javax.swing.*;
 
+/**
+ * A hierarchy frame that can take {@link NamedComponent}s in the constructor
+ * and has a {@link Navigator} so it can navigate through the components made
+ */
 public class HFrame extends JFrame {
 
-    Navigator nav;
+    private final Navigator nav;
 
+    /**
+     * Takes {@link NamedComponent}s and generates an {@link HFrame} with those components as children
+     * and their names accessible via the {@link Navigator}
+     * @param components
+     */
     public HFrame(NamedComponent... components) {
         this("", components);
     }
 
     /**
-     *
+     * Takes {@link NamedComponent}s and generates an {@link HFrame} with those components as children
+     * and their names accessible via the {@link Navigator}. It also makes the title of the frame
+     * set to the title given
      * @param title  the title of the frame
      * @param components  the components to add as children (and add to the navigator)
      */
-
     public HFrame(String title, NamedComponent... components) {
         super(title);
         nav = Navigator.getConstructed(this);
@@ -32,7 +42,6 @@ public class HFrame extends JFrame {
      *
      * @implNote  the navigator does not have components that were made after the construction of an element
      */
-
     public Navigator getNavigator() {
         return nav;
     }
